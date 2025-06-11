9ac5d3cefb0d1f4d03eee7214c9d14ec184930c8
ivanverve <ivanverve@hotmail.com>
2025-06-11 14:09:32 -0300

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
